2176b7a7c8fd18eb8528971668f7284df86ba5b6
basildevops <eldhodevops1@gmail.com>
2025-01-29 21:15:16 +0530

    Create test_model_api.py

---
+++ app/test_model_api.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+# Define API endpoint
+url = "http://127.0.0.1:5001/invocations"
+
+# Prepare input data
+data = {
+    "columns": ["Size (sq ft)"],  # Feature name
+    "data": [[1200], [2000]]      # Input data
+}
+
+# Send POST request
+response = requests.post(url, json=data)
+
+# Print response
+print("Predictions:", response.json())
